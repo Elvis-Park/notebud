dd9d65dedc3b7edb4f8f251b4ce062146edfef93
noob999-hacker <natalie027784@gmail.com>
2025-04-04 19:00:22 -0700

    made object

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print("hello")
-print("world")
+from note import Note
+
+user=input("1. add new\n" + 
+           "2. note search\n"+
+           "3. list notes\n")
+
+if user == "1":
+    pass
+
+
+
+
+
+test = Note("Testing","test unit","4/4/2025",True)
+
+print(test.desc)
---
+++ note.py
@@ -0,0 +1,6 @@
+class Note():
+    def __init__(self, title, desc, date, isimportant):
+        self.title=title
+        self.desc=desc
+        self.date=date
+        self.isimportant=isimportant
