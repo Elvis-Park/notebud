47d2972848662a51ec9fb5e96446e279214fe783
LaySChippy <tristanbonoan@icloud.com>
2025-04-18 19:00:43 -0700

    added note feature

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
+from colorama import Fore,Style,init
+from datetime import date
 from note import Note
 
-user=input("1. add new\n" + 
-           "2. note search\n"+
-           "3. list notes\n")
-
-if user == "1":
-    pass
+noteslist=[]
 
 
+while True:
+    user=input(
+            (Fore.LIGHTGREEN_EX+"1. add new\n" ) + 
+            (Fore.CYAN+"2. note search\n")+
+            (Fore.BLACK+"3. list notes\n")+
+            Style.RESET_ALL)
+
+    if user == "1":
+        t=input("Please Title this note, AI cant do everything for you: ")
+        d=input("Enter the description and stop scrolling through tiktok: ")
+        da= date.today().strftime("%m/%d/%Y")
+        ii=input("is this note important, if you say it isnt thats my dinner (y/n): ")
+
+        if ii=="y":
+            ii=True
+        else:
+            ii=False
+
+        note=Note(t,d,da,ii)
+        noteslist.append(note)
 
 
 
-test = Note("Testing","test unit","4/4/2025",True)
 
-print(test.desc)
+
+
+
+# note1 = Note("Testing","test unit","4/4/2025",True)
+# note2 = Note("test unit" , "Testing" , "68/98/2300",False)
+
+# print(note1.title)
+# print(note2.desc)
